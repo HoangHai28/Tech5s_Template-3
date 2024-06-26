565372e8c906ca3b123ef9c5836b1513e80ee3b9
HoangHai28 <hoanghai28702@gmail.com>
2024-06-26 10:46:09 +0700

    continue

--- public/js/main.js
+++ public/js/main.js
@@ -148,3 +148,15 @@ var Product = {
 document.addEventListener("DOMContentLoaded", () => {
   Product.init();
 });
+
+
+
+
+function show_hide() {  
+  var click = document.getElementById("list-items");  
+  if(click.style.display ==="none") {  
+     click.style.display ="block";  
+  } else {  
+     click.style.display ="none";  
+  }   
+}  
